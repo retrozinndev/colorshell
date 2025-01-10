5b7125b296823761252c8cb24161f3d08dada005
retrozinndev <joaovodias@gmail.com>
2025-01-09 22:53:16 -0300

    :boom: hypr(scripts): fix 'memory leak' in notification-handler-daemon script

--- hypr/scripts/notification-handler-daemon.sh
+++ hypr/scripts/notification-handler-daemon.sh
@@ -7,8 +7,8 @@
 # Made by retrozinndev (João Dias)
 # From https://github.com/retrozinndev/Hyprland-Dots
 
-eww_config_dir=$(eww get EWW_CONFIG_DIR)
 logfile="$HOME/.cache/notification-handler-daemon.log"
+handler_script="$HOME/.config/eww/scripts/notification-handler.sh"
 
 function Send_log() {
     echo "[$1]${@/$1}" >> $logfile
@@ -20,20 +20,20 @@ function Exit_daemon() {
 }
 
 function Restart_handler() {
-    sh $eww_config_dir/scripts/notification-handler.sh &
+    bash $handler_script &
     pid_handler=$!
     Send_log "info" "Handler started!"
     Send_log "info" "Handler script PID: $pid_handler"
     wait $pid_handler && Exit_daemon || \
         (Send_log "error" "An error occurred and handler stopped"
-          Send_log "info" "Clearing history and starting handler again."
-          Restart_handler)
+         Send_log "info" "Clearing history and starting handler again."
+         Restart_handler)
 }
 
 trap "Send_log 'info' 'SIGINT received, stopping daemon and handler' ; kill \$pid_handler ; exit 1" SIGINT
 trap "Send_log 'info' 'SIGTERM received, stopping daemon and handler' ; kill \$pid_handler ; exit 1" SIGTERM
 
-echo '' > $logfile
+echo -e '' > $logfile
 Send_log "info" "Starting Daemon..."
 Send_log "info" "Daemon script PID: $$"
 Restart_handler
