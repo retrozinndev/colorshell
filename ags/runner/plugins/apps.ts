9bca6de71ef88e7d710335dda52d2c7c8f2a9db7
retrozinndev <joaovodias@gmail.com>
2025-03-22 08:40:10 -0300

    :boom: ags(runner/apps): fix app icon issues

--- ags/runner/plugins/apps.ts
+++ ags/runner/plugins/apps.ts
@@ -1,8 +1,8 @@
-import AstalHyprland from "gi://AstalHyprland";
 import { ResultWidget, ResultWidgetProps } from "../../widget/runner/ResultWidget";
 import AstalApps from "gi://AstalApps";
-import { getAstalApps } from "../../scripts/apps";
+import { cleanExec, getAstalApps } from "../../scripts/apps";
 import { Runner } from "../Runner";
+import { Astal } from "astal/gtk3";
 
 export const PluginApps = {
     // Do not provide prefix, so it's always ran.
@@ -12,9 +12,9 @@ export const PluginApps = {
             new ResultWidget({
                 title: app.get_name(),
                 description: app.get_description(),
-                icon: app.iconName,
-                onClick: () => AstalHyprland.get_default().dispatch("exec", app.get_executable())
+                icon: Astal.Icon.lookup_icon(app.iconName) ? app.iconName : "application-x-executable-symbolic",
+                onClick: () => cleanExec(app)
             } as ResultWidgetProps)
-        ) || null;
+        );
     }
 } as Runner.Plugin;
