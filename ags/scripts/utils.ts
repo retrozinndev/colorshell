dbd65835e9d461d8da7cfdc47342402e373cb5ad
retrozinndev <joaovodias@gmail.com>
2025-05-11 22:12:25 -0300

    :wastebasket: ags(scripts/utils): remove unnecessary `getUserDirs()` method
    use GLib methods instead

--- ags/scripts/utils.ts
+++ ags/scripts/utils.ts
@@ -9,16 +9,6 @@ export function getHyprlandVersion(): string {
     return exec(`${GLib.getenv("HYPRLAND_CMD") || "Hyprland"} --version | head -n1`).split(" ")[1];
 }
 
-export function getUserDirs() {
-    return {
-        home: GLib.get_home_dir(),
-        state: GLib.get_user_state_dir(),
-        cache: GLib.get_user_cache_dir(),
-        config: GLib.get_user_config_dir(),
-        data: GLib.get_user_data_dir()
-    };
-}
-
 export function makeDirectory(dir: string): void {
     execAsync([ "mkdir", "-p", dir ]);
 }
@@ -28,7 +18,7 @@ export function deleteFile(path: string): void {
 }
 
 export function isInstalled(commandName: string): boolean {
-    const output = exec(["command", "-v", commandName]);
+    const output = exec(["bash", "-c", `command -v ${commandName}`]);
     if(output) 
         return true;
 
