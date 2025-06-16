a69bdfdb7c31ac98977f6475f6f024acf75e1110
Mephisto <38382271+NotMephisto@users.noreply.github.com>
2025-06-16 19:36:34 +0300

    Using Decoder for Uint8Array type data

--- ags/widget/control-center/pages/Network.ts
+++ ags/widget/control-center/pages/Network.ts
@@ -92,7 +92,7 @@ export const PageNetwork: (() => Page) = () => new Page({
                             new Widget.Label({
                                 className: "ssid",
                                 halign: Gtk.Align.START,
-                                label: ap.ssid.get_data()?.toString() ?? "Wi-Fi"
+                                label: getDecoded(ap.ssid.get_data()) ?? "Wi-Fi"
                             } as Widget.LabelProps),
                             new Widget.Label({
                                 className: "status",
