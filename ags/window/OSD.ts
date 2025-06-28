a5b893dcc8b0a3fb48748ed797903b52cd58f844
Mephisto <38382271+NotMephisto@users.noreply.github.com>
2025-06-28 21:00:40 +0300

    Update OSD.ts

--- ags/window/OSD.ts
+++ ags/window/OSD.ts
@@ -1,14 +1,15 @@
 import { bind, Binding, Variable } from "astal";
 import { Astal, Gtk, Widget } from "astal/gtk3";
 import { Wireplumber } from "../scripts/volume";
+import AstalWp from "gi://AstalWp";
 
 export enum OSDModes {
     SINK,
+    SOURCE,
     BRIGHTNESS
 }
 
 let osdMode: (Variable<OSDModes>|null);
-let osdIcon: (Binding<string | undefined>|null);
 
 export function setOSDMode(newMode: OSDModes): void {
     if(!osdMode) return;
@@ -16,18 +17,101 @@ export function setOSDMode(newMode: OSDModes): void {
     osdMode.set(newMode);
 }
 
+function createOSD(
+    props: Widget.BoxProps,
+    bindable: AstalWp.Endpoint,
+    iconName: string | Binding<string>,
+    labelOSD: string | Binding<string>,
+    valueOSD: number | Binding<number>,
+    maxValueOSD: number | Binding<number>
+
+) {
+    return new Widget.Box({
+        ...props,
+        className: `osd ${props.className || ''}`, 
+        expand: true,
+        children: [
+            new Widget.Icon({
+                className: "icon",
+                icon: iconName,
+            } as Widget.IconProps),
+            new Widget.Box({
+                className: "volume",
+                orientation: Gtk.Orientation.VERTICAL,
+                valign: Gtk.Align.CENTER,
+                children: [
+                    new Widget.Label({
+                        className: "device",
+                        label: labelOSD,
+                        truncate: true,
+                    } as Widget.LabelProps),
+                    new Widget.Box({
+                        vexpand: false,
+                        expand: false,
+                        children: [
+                            new Widget.LevelBar({
+                                className: "levelbar",
+                                width_request: 120,
+                                value: valueOSD,
+                                maxValue: maxValueOSD,
+                                expand: true,
+                            } as Widget.LevelBarProps),
+                            /*new Widget.Label({
+                                className: "value",
+                                label: bind(Wireplumber.getDefault().getDefaultSink(), "volume").as((volume: number) => 
+                                    `${Math.floor(volume * 100)}%`),
+                                vexpand: false,
+                                expand: false,
+                                halign: Gtk.Align.CENTER
+                            } as Widget.LabelProps)*/
+                        ]
+                    } as Widget.BoxProps)
+                ]
+            } as Widget.BoxProps)
+        ]
+    } as Widget.BoxProps)
+}
+
+function OSDSink() {
+    const audio = Wireplumber.getDefault().getDefaultSink();
+    return createOSD(
+        { name: "sink" },
+        audio,
+        bind(audio, "volumeIcon").as(icon => 
+            !Wireplumber.getDefault().isMutedSink() && Wireplumber.getDefault().getSinkVolume() > 0 ? 
+                icon : "audio-volume-muted-symbolic"),
+        bind(audio, "description").as((description: string) => 
+            description || "Speaker"),
+        bind(audio, "volume").as((volume: number) => 
+            Math.floor(volume * 100)),
+        bind(Wireplumber.getWireplumber(), "defaultSpeaker").as(() => 
+            Wireplumber.getDefault().getMaxSinkVolume())
+    )
+}
+
+function OSDSource() {
+    const source = Wireplumber.getDefault().getDefaultSource();
+    return createOSD(
+        { name: "source" },
+        source,
+        bind(source, "volumeIcon").as(icon => 
+            !Wireplumber.getDefault().isMutedSource() && Wireplumber.getDefault().getSourceVolume() > 0 ? 
+                icon : "microphone-sensitivity-muted-symbolic"),
+        bind(source, "description").as((description: string) => 
+            description || "Microphone"),
+        bind(source, "volume").as((volume: number) => 
+            Math.floor(volume * 100)),
+        bind(Wireplumber.getWireplumber(), "defaultMicrophone").as(() =>
+            Wireplumber.getDefault().getMaxSourceVolume())
+    )
+}
+
 export const OSD = (mon: number) => {
     osdMode = new Variable<OSDModes>(OSDModes.SINK);
-    osdIcon = osdMode().as((mode: OSDModes) => {
-        switch(mode) {
-            case OSDModes.SINK: return "󰕾";
-            case OSDModes.BRIGHTNESS: return "󰃠";
-            default: return "󱧣";
-        }
-    });
 
     return new Widget.Window({
         namespace: "osd",
+        className: "osd-window",
         layer: Astal.Layer.OVERLAY,
         anchor: Astal.WindowAnchor.BOTTOM,
         canFocus: false,
@@ -39,54 +123,21 @@ export const OSD = (mon: number) => {
             osdMode?.drop();
 
             osdMode = null;
-            osdIcon = null;
         },
-        child: new Widget.Box({
-            className: "osd",
+        child: new Widget.Stack({
+            visibleChildName: bind(osdMode, "value").as((mode: OSDModes) => {
+                switch (mode) {
+                    case OSDModes.SINK: return "sink";
+                    case OSDModes.SOURCE: return "source";
+                    default: return "sink";
+                }
+            }),
+            onDestroy: () => {
+                osdMode = null
+            },
             children: [
-                new Widget.Icon({
-                    className: "icon",
-                    icon: bind(Wireplumber.getDefault().getDefaultSink(), "volumeIcon").as(icon => 
-                        !Wireplumber.getDefault().isMutedSink() && Wireplumber.getDefault().getSinkVolume() > 0 ? icon : "audio-volume-muted-symbolic"),
-                } as Widget.IconProps),
-                new Widget.Box({
-                    className: "volume",
-                    orientation: Gtk.Orientation.VERTICAL,
-                    valign: Gtk.Align.CENTER,
-                    children: [
-                        new Widget.Label({
-                            className: "device",
-                            label: bind(Wireplumber.getDefault().getDefaultSink(), "name").as((name: string) => 
-                                name || "Speaker"),
-                            halign: Gtk.Align.CENTER
-                        } as Widget.LabelProps),
-                        new Widget.Box({
-                            vexpand: false,
-                            expand: false,
-                            children: [
-                                new Widget.LevelBar({
-                                    className: "levelbar",
-                                    width_request: 120,
-                                    value: bind(Wireplumber.getDefault().getDefaultSink(), "volume").as((volume: number) => 
-                                        Math.floor(volume * 100)),
-                                    maxValue: bind(Wireplumber.getWireplumber(), "defaultSpeaker").as(() =>
-                                        Wireplumber.getDefault().getMaxSinkVolume()),
-                                    vexpand: false,
-                                    expand: false,
-                                    halign: Gtk.Align.CENTER
-                                } as Widget.LevelBarProps),
-                                /*new Widget.Label({
-                                    className: "value",
-                                    label: bind(Wireplumber.getDefault().getDefaultSink(), "volume").as((volume: number) => 
-                                        `${Math.floor(volume * 100)}%`),
-                                    vexpand: false,
-                                    expand: false,
-                                    halign: Gtk.Align.CENTER
-                                } as Widget.LabelProps)*/
-                            ]
-                        } as Widget.BoxProps)
-                    ]
-                } as Widget.BoxProps)
+                OSDSink(),
+                OSDSource(),
             ]
         } as Widget.BoxProps)
     } as Widget.WindowProps);
