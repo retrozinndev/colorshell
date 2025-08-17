1fa6c018a461a6db44545517a1728071eb3f97d2
retrozinndev <joaovodias@gmail.com>
2025-08-17 20:15:03 -0300

    :wrench: chore(hypr/scripts/exec): support executing desktop entries by name

--- config/hypr/scripts/exec.sh
+++ config/hypr/scripts/exec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script executes the provided program with UWSM
-# if in usage or launches it normally with hyprctl.
+# if active, or else normally.
 # ---------------
 # Licensed under the MIT License
 # Made by retrozinndev (João Dias)
@@ -10,6 +10,11 @@
 
 if uwsm check is-active; then
     exec uwsm app -- "$@"
+    exit 0
+fi
+
+if [[ $1 =~ [.]desktop$ ]]; then
+    gtk-launch $@
     exit 0
 fi
 
