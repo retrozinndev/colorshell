14a83ea05dba234a5751a11356a54f8c1428a430
retrozinndev <joaovodias@gmail.com>
2026-02-28 23:03:07 -0300

    :wrench: chore(scripts/socket): check if `.pid` file exists before reading it

--- scripts/socket.sh
+++ scripts/socket.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if ps -p `cat "$XDG_RUNTIME_DIR/colorshell/.pid"` > /dev/null; then
+_pidfile=${XDG_RUNTIME_DIR:-"/run/user/`id -u`"}/colorshell/.pid
+
+if [[ -f $_pidfile ]] && ps -p `cat "$_pidfile"` > /dev/null; then
 
     if command -v socat > /dev/null 2>&1; then
         echo "$@" | socat - "${XDG_RUNTIME_DIR:-"/run/user/$(id -u)"}/colorshell/.sock"
