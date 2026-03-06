d7607b25459f4e4763ae1e4d3b20dc802cd912b4
retrozinndev <joaovodias@gmail.com>
2026-03-06 11:02:25 -0300

    :boom: fix(compositors/hyprland): do not apply shell bind if user already set one to the same combination
    fixes #46

--- src/modules/compositors/hyprland.ts
+++ src/modules/compositors/hyprland.ts
@@ -4,9 +4,9 @@ import { register } from "ags/gobject";
 import AstalHyprland from "gi://AstalHyprland";
 import GLib from "gi://GLib?version=2.0";
 import { Socket } from "../socket";
-import { exec } from "ags/process";
+import { exec, execAsync } from "ags/process";
 import Gio from "gi://Gio?version=2.0";
-import { decoder, makeDirectory, playSystemBell } from "../utils";
+import { decoder, playSystemBell } from "../utils";
 import { Shell } from "../../app";
 
 
@@ -24,8 +24,7 @@ export class CompositorHyprland extends Compositor {
         if(instSignature === null || instSignature.trim() === "")
             throw new Error("Compositor: Hyprland: Couldn't get instance signature");
 
-        this.loadConfigs();
-
+        this.initConfig();
         this.#eventSock = new Socket(
             Socket.Type.CLIENT,
             `${GLib.get_user_runtime_dir()}/hypr/${instSignature}/.socket2.sock`,
@@ -92,7 +91,7 @@ export class CompositorHyprland extends Compositor {
             case "configreloaded":
                 if(!this.#ignoreConfigReload) {
                     this.#ignoreConfigReload = true;
-                    this.source();
+                    this.reload();
                     return;
                 }
 
@@ -110,75 +109,128 @@ export class CompositorHyprland extends Compositor {
     }
 
     
-    private source(): void {
+    private source(path: string): void {
+        if(!path.endsWith(".conf"))
+            return;
+
+        try {
+            const out = exec(
+                `hyprctl keyword source "${path}"`
+            );
+
+            !/^ok.*$/.test(out) &&
+                console.error(out);
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    private reload(): void {
         const names = Gio.resources_enumerate_children(
             "/io/github/retrozinndev/colorshell/config/hyprland",
-            Gio.ResourceLookupFlags.NONE
-        );
+            null
+        ).filter(name => !name.includes("bindings"));
+
         exec("hyprctl reload");
-        names.forEach(name => {
-            if(!name.endsWith(".conf"))
+        names.forEach(name => this.source(`${this.#configDir.peek_path()!}/${name}`));
+        this.loadBinds();
+    }
+
+
+    /** loads shell-specific binds from the binds.conf file packed into gresource.
+      * this smartly excludes binds that the user has already set, avoiding duplicate binds */
+    private loadBinds(): void {
+        type Bind = { params: string, flags?: string, modmask: number, key: string };
+        const bindingsConf = decoder.decode(Gio.resources_lookup_data(
+            "/io/github/retrozinndev/colorshell/config/hyprland/bindings.conf",
+            null
+        )?.toArray());
+        const bindExpr = /^bind([lrcgoenmtisdpu]*)?[ ]+=[ ]+(.*)$/;
+        const binds: Array<Bind> = [];
+
+
+        for(const line of bindingsConf.split('\n')) {
+            if(/^#/.test(line.trim()) || /^\n$/.test(line.trim()))
+                continue;
+
+            if(!bindExpr.test(line))
+                continue;
+
+            const [, flags, params] = bindExpr.exec(line) ?? [];
+
+            if(!params) // skip if there are no params
+                continue;
+
+            const bind = {
+                params: params.replace(/, /g, ","),
+                flags: flags ?? undefined,
+                modmask: 0
+            } as Bind;
+
+            binds.push(bind);
+
+            const [modkey, key] = params.split(',');
+            if(modkey !== undefined) 
+                bind.modmask = this.modkeyToModmask(modkey.toLowerCase());
+
+            if(key !== undefined)
+                bind.key = key.toLowerCase();
+        }
+
+        binds.forEach(bind => {
+            const userBinds = AstalHyprland.get_default().get_binds();
+            const match = userBinds.find(b => b.modmask === bind.modmask && 
+                b.key.trim().toLowerCase() === bind.key.trim().toLowerCase()
+            );
+
+            if(match)
                 return;
 
-            try {
-                const out = exec(
-                    `hyprctl keyword source ${this.#configDir.peek_path()!}/${name}`
-                );
-                !/^ok.*$/.test(out) && console.log(out)
-            } catch(e) {
-                console.error(e);
-            }
+            // unbind just in case
+            execAsync(`hyprctl keyword unbind "${bind.params.split(',').toSpliced(0, 2)}"`)
+                .catch(console.error);
+            execAsync(`hyprctl keyword bind${bind.flags ?? ""} "${bind.params}"`)
+                .catch(console.error);
         });
     }
 
     /** load necessary hyprland configs from gresource */
-    private loadConfigs(): void {
+    private initConfig(): void {
         const userLastUpdatedFile = Gio.File.new_for_path(`${this.#configDir.peek_path()!}/.last-updated`);
         const names = Gio.resources_enumerate_children(
             "/io/github/retrozinndev/colorshell/config/hyprland",
             null
-        );
+        ).filter(name => !name.includes("bindings"));
 
         if(userLastUpdatedFile.query_exists(null)) {
-            // check if data is different
+            const userLastUpdated = decoder.decode(userLastUpdatedFile.read(null).read_bytes(32, null).toArray());
             const configlastUpdated = decoder.decode(Gio.resources_lookup_data(
                 "/io/github/retrozinndev/colorshell/config/hyprland/.last-updated", null
             ).toArray());
 
-            const userLastUpdated = decoder.decode(userLastUpdatedFile.read(null).read_bytes(32, null).toArray());
-
+            // check if data is different
             if(configlastUpdated === userLastUpdated) {
-                this.source();
+                this.reload();
                 return; // no need to update, since it's unchanged/same
             }
         }
 
-        const files = names.map(name => [
-            name,
-            Gio.resources_lookup_data(
+        names.forEach(name => {
+            const file = Gio.File.new_for_path(`${this.#configDir.peek_path()!}/${name}`);
+            const data = Gio.resources_lookup_data(
                 `/io/github/retrozinndev/colorshell/config/hyprland/${name}`,
                 null
-            )
-        ] satisfies [string, GLib.Bytes]);
-
-        makeDirectory(`${Shell.runtimeDir.peek_path()!}/config/hyprland`);
-        files.forEach(([name, data]) => {
-            const file = Gio.File.new_for_path(`${this.#configDir.peek_path()!}/${name}`);
+            );
 
             try {
-                !file.query_exists(null) && 
-                    file.create(null, null);
-
-                file.replace_contents(
-                    data.toArray(), null, false, Gio.FileCreateFlags.REPLACE_DESTINATION, null
-                );
+                file.replace_contents(data.toArray(), null, false, Gio.FileCreateFlags.REPLACE_DESTINATION, null);
             } catch(e) {
                 console.error(`Compositor: Hyprland: Failed to write config file "${name}": ${(e as Error).message}`);
                 console.debug(e);
             }
         });
 
-        this.source();
+        this.reload();
     }
 
     private getActiveClient(): CompositorHyprland.Client|null {
@@ -188,6 +240,36 @@ export class CompositorHyprland extends Compositor {
             return null;
 
         return client as CompositorHyprland.Client;
+    }
+
+    private modkeyToModmask(key: string): number {
+        const mods = ["shift", "caps|capslock", "ctrl|control", "alt", "mod2", "mod3", "super|meta", "mod5"];
+        const modmasks: Array<number> = [];
+
+        key.split(' ').forEach((key, i) => {
+            const modkeyIndex = mods.findIndex(k => k.includes(key.toLowerCase()));
+
+            if(modkeyIndex < 0 || key.trim() === "") {
+                modmasks[i] = 0;
+                return;
+            }
+
+            modmasks[i] = 1 << modkeyIndex;
+            console.log(key, modmasks[i]);
+        });
+
+        let mask: number|undefined = undefined;
+        for(let i = 0; i < modmasks.length; i++) {
+            const mm = modmasks[i];
+            mask ??= mm;
+
+            if(i === (modmasks.length-1))
+                return mask !== mm ? mask : (mask | mm);
+
+            mask = mask | mm; // we do a bitwise OR to join 'em!!
+        }
+
+        return mask ?? 0;
     }
 }
 
