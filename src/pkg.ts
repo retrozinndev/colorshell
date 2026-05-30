7996aa56fd1c7eb52039ea93cfe35ca9a09360a4
retrozinndev <joaovodias@gmail.com>
2026-05-30 14:18:14 -0300

    :wrench: chore(pkg): bring back package configuration, use global `pkg`
    this is still being used in gtk4

--- src/pkg.ts
+++ src/pkg.ts
@@ -1,6 +1,30 @@
 import "./overrides"; // thanks Aylur!!
 import I18n from "./i18n/intl";
 
+
+pkg.init({
+    name: "io.github.retrozinndev.Colorshell",
+    version: VERSION,
+    prefix: "~/.local",
+    datadir: "/share",
+    libdir: "/lib"
+});
+
+pkg.require({
+    "Adw": "1",
+    "Gly": "2",
+    "GlyGtk4": "2",
+    "Astal": "4.0",
+    "AstalBattery": "0.1",
+    "AstalApps": "0.1",
+    "AstalBluetooth": "0.1",
+    "AstalHyprland": "0.1",
+    "AstalMpris": "0.1",
+    "AstalNetwork": "0.1",
+    "AstalNotifd": "0.1",
+    "AstalTray": "0.1",
+    "AstalWp": "0.1",
+});
 Object.assign(globalThis, {
     assert(...conditions: Array<any>): boolean {
         if(!conditions.every(v => Boolean(v)))
