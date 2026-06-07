cc2c2af65f3622a4236faa57c8d7c1da8058fdb8
lucagoc <77584602+lucagoc@users.noreply.github.com>
2026-06-07 11:09:53 +0900

    💥 fix(widget): prevent full-screen app search crash when app icon is null

--- src/widget/AppIcon.tsx
+++ src/widget/AppIcon.tsx
@@ -49,7 +49,7 @@ class AppIcon extends Gtk.Image {
         });
 
         if(props.icon !== undefined)
-            this.icon = typeof props.icon === "object" ?
+            this.icon = (props.icon && typeof props.icon === "object") ?
                 props.icon.peek_path()!
             : props.icon;
     }
@@ -58,7 +58,7 @@ class AppIcon extends Gtk.Image {
 namespace AppIcon {
     export interface SignalSignatures extends Gtk.Image.SignalSignatures {}
     export interface ConstructorProps extends Gtk.Image.ConstructorProps {
-        icon: string|Gio.File;
+        icon: string|Gio.File|null;
     }
 }
 
